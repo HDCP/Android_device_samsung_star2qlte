06e450924ba754d141b1b8df04259931f175c30d
Sasukezin678 <carlosjunior1525@gmail.com>
2022-10-28 07:28:22 +0000

    fix source for android 12.1 support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,5 @@ TARGET_OTA_ASSERT_DEVICE := star2qltechn
 TARGET_KERNEL_CONFIG := star2qlte_chn_open_defconfig
 
 # Vendor init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_star2qltechn
-TARGET_RECOVERY_DEVICE_MODULES := libinit_star2qltechn
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_samsung_star2qltechn
+TARGET_RECOVERY_DEVICE_MODULES := init_samsung_star2qltechn
