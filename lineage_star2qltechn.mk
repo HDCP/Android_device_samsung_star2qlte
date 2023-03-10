5127b5ef48f3b6c09eaeea4db14eb4667a6b0cec
HDCP <HDCP@users.noreply.github.com>
2023-03-09 21:06:52 -0500

    Update lineage_star2qltechn.mk

--- lineage_star2qltechn.mk
+++ lineage_star2qltechn.mk
@@ -20,15 +20,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit from star2qltechn device
-$(call inherit-product, device/samsung/star2qltechn/device.mk)
+$(call inherit-product, device/samsung/star2qlte/device.mk)
 
 # Inherit some common Lineageos stuff (aosp default apps such as camera, phone, latinIME, etc)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 
 # Device identifier
-PRODUCT_NAME := lineage_star2qltechn
-PRODUCT_DEVICE := star2qltechn
+PRODUCT_NAME := lineage_star2qlte
+PRODUCT_DEVICE := star2qlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G9650
 PRODUCT_MANUFACTURER := samsung
